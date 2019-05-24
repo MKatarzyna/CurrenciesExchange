a9bc1e1b4ab18660713aff82e5ba9c6ccc2d227c
Katarzyna Mural <katarzyna.mural@gmail.com>
2019-05-24 20:24:25 +0200

    added chart

--- ExchangeCurrencies/src/chart.js
+++ ExchangeCurrencies/src/chart.js
@@ -1,60 +1,61 @@
-
 const renderChart = async (currencyFrom, currencyTo) => {
 
-// var CurrencyInputFrom = document.getElementById('currencyFrom').value;
-// var CurrencyInputTo = document.getElementById('currencyTo').value;
-// console.log(CurrencyInputFrom);
+	var dps = []; // dataPoints
 
-var dps = await fetch('https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=' + currencyFrom + '&to_symbol=' + currencyTo + '&interval=5min&apikey=EQGG2IE4E1FDHPS6').then(data => data.json());
+	var chart = new CanvasJS.Chart("chartContainer", {
+		title :{
+			text: "Historical exchange chart"
+		},
+		axisY: {
+			includeZero: false
+		},      
+		data: [{
+			type: "line",
+			dataPoints: dps
+		}]
+	});
+	
+	var xVal = 0;
+	var yVal = 10; 
+	var updateInterval = 6000;
+	var amount = 12;
+	var dataChartValueFX = await fetch('https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=' + currencyFrom + '&to_symbol=' + currencyTo + '&interval=60min&apikey=EQGG2IE4E1FDHPS6').then(data => data.json());
 
-console.log(dps);
-
-// var chart = new CanvasJS.Chart("chartContainer", {
-// 	// animationEnabled: true,
-// 	theme: "light2",
-// 	title:{
-// 		text: "Historical exchange chart"
-// 	},
-// 	axisX:{
-// 		includeZero: false
-// 	},
-// 	axisY:{
-// 		includeZero: false
-// 	},
-// 	data: [{        
-// 		type: "line",       
-// 		dataPoints: dps
-// 	}]
-// });
-
-
-// var xVal = 0;
-// var yVal = 100; 
-// var updateInterval = 1000;
-// var dataLength = 20; // number of dataPoints visible at any point
-
-// var updateChart = function (count) {
-
-// 	count = count || 1;
-
-// 	for (var j = 0; j < count; j++) {
-// 		yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
-// 		dps.push({
-// 			x: xVal,
-// 			y: yVal
-// 		});
-// 		xVal++;
-// 	}
-
-// 	if (dps.length > dataLength) {
-// 		dps.shift();
-// 	}
-
-	// chart.render();
-// };
-
-// updateChart(dataLength);
-// setInterval(function(){updateChart()}, updateInterval);
-
-
+	
+	const updateChart = function () {
+		console.log(dataChartValueFX['Time Series FX (60min)']);
+		console.log(amount);
+		
+		var i;
+		for(i = 0; i < amount; i++) {
+			var date = Object.keys(dataChartValueFX['Time Series FX (60min)'])[i];
+			// console.log(dataChartValueFX['Time Series FX (5min)'][date]['1. open']);
+			
+			yVal = parseFloat(dataChartValueFX['Time Series FX (60min)'][date]['1. open']);
+			
+			console.log(yVal);
+			console.log(xVal);
+			
+			dps.push({
+				x: xVal,
+				y: yVal
+			});
+			xVal++;
+			
+			if (dps.length > amount) {
+				dps.shift();
+			}
+			
+			chart.render();
+		}
+	}
+	
+	const updateData = async (currencyFrom, currencyTo) => {
+		dataChartValueFX = await fetch('https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=' + currencyFrom + '&to_symbol=' + currencyTo + '&interval=60min&apikey=EQGG2IE4E1FDHPS6').then(data => data.json());
+		
+		console.log(dataChartValueFX['Time Series FX (60min)']);
+	}
+	
+	updateChart();
 }
+
